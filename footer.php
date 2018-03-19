c58e400318dba0702865e747e499b0807558de84
Hakeem <hhaakkimm@gmail.com>
2018-03-19 15:25:20 +0600

    Footer2

--- footer.php
+++ footer.php
@@ -3,19 +3,27 @@
 ?>
 
 <style>
-  .jumbotronTransp {
-     background-image: url("images/fon225.jpg");
-         padding: 30px;
-         margin-bottom: 10px;
-         font-size: 21px;
-         font-weight: 200;
-         line-height: 1;
-         color: inherit;
-         background-color: transparent;
-      }
-  .tt{
-    text-align: center;
-  }
+.suret
+{
+   background-image: url("images/fon225.jpg");
+   background-attachment: fixed;
+   /* filter: brightness(50%); */
+}
+.jumbotronTransp {
+       padding: 30px;
+       margin-bottom: 10px;
+       font-size: 21px;
+       font-weight: 200;
+       line-height: 1;
+       color: inherit;
+       background-color: rgba(0,0,0,0.5);
+       padding-bottom: 100px;
+    }
+    .tt{
+      text-align: center;
+      color: white;
+
+    }
   #sub{
         position: absolute;
         right: 15px;
@@ -27,28 +35,30 @@
     <!--scripts-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <div class="jumbotronTransp">
-      <div class="row">
-      <div class="col-sm-2"></div>
-      <div class="col-sm-8">
-        <div class="tt">
-          <h1>CONTACT US</h1>
-          <hr>
-          <h5>If you want to know more about our services, please fill up our inquiry form so that we can respond to you as soon as possible.</h5>
-        </div>
-        <form>
-          <div class="form-group">
-          <textarea class="form-control" rows="1" id="name" placeholder="NAME*"></textarea><br>
-          <textarea class="form-control" rows="1" id="email" placeholder="EMAIL ADDRESS*"></textarea><br>
-          <textarea class="form-control" rows="1" id="subject" placeholder="SUBJECT"></textarea><br>
-          <textarea class="form-control" rows="5" id="comment" placeholder="MESSAGE"></textarea>
-          </div>
-          <button id="sub" type="submit" class="btn btn-default">Submit</button>
-        </form>
-      </div>
+    <div class="suret">
+      <div class="jumbotronTransp">
+        <div class="row">
         <div class="col-sm-2"></div>
-      </div>
+        <div class="col-sm-8">
+          <div class="tt">
+            <h1><span style="font-weight: 400">CONTACT </span>US</h1>
+            <hr>
+            <h5><small style="color:white">If you want to know more about our services, please fill up our inquiry form so that we can respond to you as soon as possible.</small></h5>
+          </div>
+          <form>
+            <div class="form-group">
+            <textarea class="form-control" rows="1" id="name" placeholder="NAME*"></textarea><br>
+            <textarea class="form-control" rows="1" id="email" placeholder="EMAIL ADDRESS*"></textarea><br>
+            <textarea class="form-control" rows="1" id="subject" placeholder="SUBJECT"></textarea><br>
+            <textarea class="form-control" rows="5" id="comment" placeholder="MESSAGE"></textarea>
+            </div>
+            <button id="sub" type="submit" class="btn btn-default">Submit</button>
+          </form>
+        </div>
+          <div class="col-sm-2"></div>
+        </div>
 
+      </div>
     </div>
 
 <br><br><br>
